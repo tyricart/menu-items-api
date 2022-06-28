d2a09a1831d97f107f3b67e7afc72c1204d89d8e
Tyrene Ricart <tyrenericart@Tyrenes-MacBook-Pro.local>
2022-06-27 20:43:27 -0400

    menu-items

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('it is working')
+// getting express library 
+const express = require('express')
+const menuData = require('./menu-items.json')
+const cors = require('cors')
+
+// giving express the name of app
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(4000, () => {
+    console.log('Our API is listening on port 4000 - YESS it is working')
+})
+
+app.get('/', (request, response) => {
+    response.send('My API is running!!!')
+})
+
+app.get('/menu-items', (request, response) => {
+    response.send(menuData)
+})
